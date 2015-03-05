torvalds/linux

arm: socfpga: Arria 10 SOC's cpu1-start address is 3 bytes wide
Add another nibble for the cpu1-start-addr mask to support Arria 10 SOC.

Signed-off-by: Dinh Nguyen <dinguyen@opensource.altera.com>

Conflicts:

	arch/arm/mach-socfpga/platsmp.c

arch/arm/mach-socfpga/platsmp.c
@@ -41,7 +41,7 @@ static int __cpuinit socfpga_boot_secondary(unsigned int cpu, struct task_struct
 		memcpy(phys_to_virt(0), &secondary_trampoline, trampoline_size);
 
 		__raw_writel(virt_to_phys(socfpga_secondary_startup),
-			(sys_manager_base_addr + (cpu1start_addr & 0x000000ff)));
+			(sys_manager_base_addr + (cpu1start_addr & 0x00000fff)));
 
 		flush_cache_all();
 		smp_wmb();

zaqc/linux-socfpga

arch/arm/mach-socfpga/platsmp.c
@@ -41,7 +41,7 @@ static int socfpga_boot_secondary(unsigned int cpu, struct task_struct *idle)
 		memcpy(phys_to_virt(0), &secondary_trampoline, trampoline_size);
 
 		__raw_writel(virt_to_phys(socfpga_secondary_startup),
-			(sys_manager_base_addr + (socfpga_cpu1start_addr & 0x000000ff)));
+			(sys_manager_base_addr + (socfpga_cpu1start_addr & 0x00000fff)));
 
 		flush_cache_all();
 		smp_wmb();