54fd48141b4a217c8259c88f7b8e13471b1f9c22
Dinh Nguyen <dinguyen@opensource.altera.com>
2015-03-05 15:46:40 -0600

    FogBugz #282531-1: Add "pwr-en" dts property to enable the PWREN bit
    The "pwr-en" property can be used to determine if the SD power enable line
    should be turned on. In order to support the Arria10 Rev. A devkit, we
    should be turning on/off PWREN that is cued off of "pwr-en".
    
    This patch will assume that "pwr-en" is 1 when the property is not
    populated, which will be the case for Cyclone5/Arria5 based devkits. For
    the Arria10 Revision A devkit, which has a reverse PWREN logic, so we want
    to leave the PWREN bit to 0, thus we need "pwr-en = <0>".
    
    Signed-off-by: Dinh Nguyen <dinguyen@opensource.altera.com>

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -949,6 +949,7 @@ static void dw_mci_set_ios(struct mmc_host *mmc, struct mmc_ios *ios)
 {
 	struct dw_mci_slot *slot = mmc_priv(mmc);
 	const struct dw_mci_drv_data *drv_data = slot->host->drv_data;
+	struct dw_mci *host = slot->host;
 	u32 regs;
 	int ret;
 
@@ -1011,10 +1012,11 @@ static void dw_mci_set_ios(struct mmc_host *mmc, struct mmc_ios *ios)
 				slot->host->vqmmc_enabled = true;
 		}
 		set_bit(DW_MMC_CARD_NEED_INIT, &slot->flags);
-		regs = mci_readl(slot->host, PWREN);
-		regs |= (1 << slot->id);
-		/* TEMP HACK for A10. Don't turn on the PWREN. */
-		/*mci_writel(slot->host, PWREN, regs);*/
+		if (host->pdata->pwr_en) {
+			regs = mci_readl(slot->host, PWREN);
+			regs |= (1 << slot->id);
+			mci_writel(slot->host, PWREN, regs);
+		}
 		break;
 	case MMC_POWER_OFF:
 		if (!IS_ERR(mmc->supply.vmmc))
@@ -1024,10 +1026,11 @@ static void dw_mci_set_ios(struct mmc_host *mmc, struct mmc_ios *ios)
 			regulator_disable(mmc->supply.vqmmc);
 			slot->host->vqmmc_enabled = false;
 		}
-
-		regs = mci_readl(slot->host, PWREN);
-		regs &= ~(1 << slot->id);
-		mci_writel(slot->host, PWREN, regs);
+		if (host->pdata->pwr_en) {
+			regs = mci_readl(slot->host, PWREN);
+			regs &= ~(1 << slot->id);
+			mci_writel(slot->host, PWREN, regs);
+		}
 		break;
 	default:
 		break;
@@ -2473,6 +2476,7 @@ static struct dw_mci_board *dw_mci_parse_dt(struct dw_mci *host)
 	const struct dw_mci_drv_data *drv_data = host->drv_data;
 	int idx, ret;
 	u32 clock_frequency;
+	int pwr_en;
 
 	pdata = devm_kzalloc(dev, sizeof(*pdata), GFP_KERNEL);
 	if (!pdata) {
@@ -2510,6 +2514,11 @@ static struct dw_mci_board *dw_mci_parse_dt(struct dw_mci *host)
 
 	if (of_find_property(np, "supports-highspeed", NULL))
 		pdata->caps |= MMC_CAP_SD_HIGHSPEED | MMC_CAP_MMC_HIGHSPEED;
+
+	if (of_property_read_u32(dev->of_node, "pwr-en", &pwr_en)) {
+		dev_dbg(dev, "couldn't determine pwr-en, assuming pwr-en = 1\n");
+		pdata->pwr_en = 1;
+	}
 
 	return pdata;
 }
